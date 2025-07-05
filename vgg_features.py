740724ee0a0ac2788f5855bce66638355007be67
mehatab-jan <mehatab661@gmail.com>
2025-07-05 16:39:34 +0530

    Create vgg_features.py

---
+++ vgg_features.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Model
+import numpy as np
+
+def load_model():
+    base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    model = Model(inputs=base_model.input, outputs=base_model.output)
+    return model
+
+def extract_features(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    features = model.predict(x)
+    return features.flatten()
